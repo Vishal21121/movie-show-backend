9b2245a8d06eb0c7b664502039623f5ef17e01e6
Vishal21121 <vs84077000@gmail.com>
2025-02-15 20:04:47 +0530

    feat: added logger

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
+import logger from "./logger/logger.js";
 
+const morganFormat = ":method :url :status :response-time ms";
 const app = express();
 
 app.use(
@@ -9,6 +12,23 @@ app.use(
     credentials: true,
     // for legacy browsers as they choke on getting 204 for options request hence providing 200
     optionsSuccessStatus: 200,
+  })
+);
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        console.log("message: ", message);
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
   })
 );
 
